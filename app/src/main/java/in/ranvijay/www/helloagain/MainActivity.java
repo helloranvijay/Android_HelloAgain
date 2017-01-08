1b7b2eb2c5241de8251411c70e0e01eac868eb4f
Ranvijay <ranvijaykumarb@gmail.com>
2017-01-08 17:17:11 +0530

    commit after testing on real device

--- app/src/main/java/in/ranvijay/www/helloagain/MainActivity.java
+++ app/src/main/java/in/ranvijay/www/helloagain/MainActivity.java
@@ -2,6 +2,9 @@ package in.ranvijay.www.helloagain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //manually added
+        final TextView firstTextView=(TextView) findViewById(R.id.textView);
+        Button firstButton=(Button) findViewById(R.id.firstbutton);
+
+        firstButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                firstTextView.setText("You Clicked");
+            }
+        });
     }
 }
